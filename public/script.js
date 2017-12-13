aa9b71186da7ef56ab4541a70a64ea48edeebc9a
Jose Moreno <morenojr90@yahoo.com>
2017-12-13 13:06:18 -0800

    add ajax request to get pokemon, then take list and render div with pokemon information from api, add "edit", "delete", & "create pokemon" buttons.

--- public/script.js
+++ public/script.js
@@ -1,7 +1,26 @@
 console.log("Sanity Check: JS is working!");
-
 $(document).ready(function(){
-
-  // code in here
-
+  const $pokemon = $("#pokemon");
+  let isClicked = false;
+  //listens for click on pokemon button then makes GET request to mutably app.
+  $pokemon.on("click", () => {
+    $.ajax("http://mutably.herokuapp.com/pokemon")
+      //promise then returns pokemon objects
+      .then( result => {
+        result.pokemon.forEach((pokemon) => {
+          $("#resource")
+            .append("\
+              <div style='border:solid'>\
+                <p><strong>ID:</strong> " + pokemon._id +"</p>\
+                </p><strong>Pokemon Name:</strong> " + pokemon.name + "</p>\
+                <p><strong>Evolves From:</strong>" + pokemon.evolves_from +
+                "<p><strong>Pokedex:</strong>" + pokemon.pokedex + "<p>Cover:</p>\
+                <img style='width:60px;height:60px;' src='" + pokemon.image + "'>" +
+                "<p>Version: " + pokemon.__v + "</p>\
+                <button class='btn btn-sucess'>Edit</button>\
+                <button class='btn btn-danger' id='delete'>Delete</button>\
+              </div>")
+      })
+    })
+  })
 });
